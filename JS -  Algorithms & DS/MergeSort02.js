644aba0554cad3db55db91aa779a61710548fab4
Shmuel <samuel.sahala@ricult.com>
2020-12-05 17:47:26 +0200

    merge sort

---
+++ JS -  Algorithms & DS/MergeSort02.js
@@ -0,0 +1,41 @@
+// TODO: its time to break up the array into halves until you have arrays
+// that empty or have one element
+// once you have smaller sorted arrays, merge those arrays with other
+// sorted arrays until you are back at the full length of the array
+// once the array has been merged back together, return the merged(and sorted!) array.
+
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
+    }
+    if (arr2[j] < arr1[i]) {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
+function mergeSort(arr) {
+  //base case
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort([10, 24, 76, 9]));
