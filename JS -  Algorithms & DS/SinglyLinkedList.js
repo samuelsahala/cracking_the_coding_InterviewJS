277e64c051aab5a2da888aa2ef408bc56875b891
Shmuel <samuel.sahala@ricult.com>
2020-12-09 14:40:01 +0200

    singly linked list

--- JS -  Algorithms & DS/SinglyLinkedList.js
+++ JS -  Algorithms & DS/SinglyLinkedList.js
@@ -24,9 +24,95 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      // reset if this is the last item
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = temp.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let counter = 0;
+    this.current = this.head;
+    while (counter !== index) {
+      this.current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val); // !! - cov to bool
+    if (index === 0) return !!this.unshift(val); // !! - cov to bool
+    let prev = this.get(index - 1);
+    let newNode = new Node(val);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return !!this.pop();
+    let prev = this.get(index - 1);
+    let removed = prev.next;
+    prev.next = removed.next;
+    this.length--;
+    return removed;
+  }
 }
 
 let list = new SinglyLinkedList();
-list.push("hello");
-list.push("you");
+// list.push("hello");
+// list.pop("you");
 console.log(list);
