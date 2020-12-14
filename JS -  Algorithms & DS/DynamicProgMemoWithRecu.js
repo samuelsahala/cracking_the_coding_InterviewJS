e32fe5b2757d19d6f7571b4a59c03e28e33c289a
Shmuel <samuel.sahala@ricult.com>
2020-12-14 14:54:51 +0200

    tabulation solution

--- JS -  Algorithms & DS/DynamicProgMemoWithRecu.js
+++ JS -  Algorithms & DS/DynamicProgMemoWithRecu.js
@@ -10,3 +10,12 @@ function fib(n, memo = []) {
   memo[n] = res;
   return res;
 }
+
+function fib_table(n) {
+  if (n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
