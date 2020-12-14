21f4aae08b5994eae60a99d8cb0903147376faf1
Shmuel <samuel.sahala@ricult.com>
2020-12-14 14:39:18 +0200

    dynamic programming with memoization

---
+++ JS -  Algorithms & DS/DynamicProgMemoWithRecu.js
@@ -0,0 +1,12 @@
+// Dynamic Programming (not really)
+// Overlapping Subproblems
+// Optimal Substructre
+// Memoization
+
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  let res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
